3e6140bfb1568215255c3c8783ff9162a2aa5bfc
adrian-szwejk <aszwe2@illinois.edu>
2023-10-24 18:03:58 -0500

    added riverpod arch

---
+++ campus_connect/lib/presentation/feed_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class FeedScreen extends StatefulWidget {
+  const FeedScreen({super.key});
+
+  @override
+  State<FeedScreen> createState() => _FeedScreenState();
+}
+
+class _FeedScreenState extends State<FeedScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Feed'),
+      ),
+      body: const Center(
+        child: Text('Feed'),
+      ),  
+      bottomNavigationBar: BottomNavigationBar(
+        items: [],
+      ),
+    );
+  }
+}
